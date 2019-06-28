e0a063ce85673f7fa79031c458f388e767f35dc3
Saerom <saerombang11@gmail.com>
2019-06-28 16:34:46 +0900

    added button inside card

--- src/Components/ActionButton.js
+++ src/Components/ActionButton.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import Icon from '@material-ui/core/Icon';
+import Textarea from 'react-textarea-autosize';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import { flexbox } from '@material-ui/system';
+
+
 
 
 class ActionButton extends Component {
 
     state = {
-        formOpen: false
+        formOpen: false,
+        text: ''
     }
 
     openForm=()=>{
@@ -13,7 +20,18 @@ class ActionButton extends Component {
             formOpen: true
         })
     }
+
+    closeForm=(e)=>{
+        this.setState({
+            formOpen: false
+        });
+    }
     
+    handleInputChange=(e)=> {
+        this.setState({
+            text: e.target.value
+        })
+    }
 
     AddButton=()=> {
         const{list} = this.props;
@@ -39,7 +57,36 @@ class ActionButton extends Component {
     };
 
     renderForm=()=> {
-        return <p>Hello</p>;
+        const {list} = this.props;
+        const placeholder = list ? 'Enter list title' : 'Enter a title for this card';
+        const buttonTitle = list ? 'Add list' : 'Add Card'
+        return <div>
+            <Card style ={{
+                overflow: 'visible',
+                minHeight: 80,
+                minWidth: 272,
+                padding: '6px 8px 2px'
+            }}>
+                <Textarea placeholder = {placeholder}
+                autoFocus onBlur= {this.closeForm}
+                value = {this.state.value}
+                onChange = {this.handleInputChange}
+                style = {{
+                    resize: 'none',
+                    width: '100%',
+                    overflow: 'hidden',
+                    boarder: 'none',
+                    outline: 'none',
+                }}
+                />
+            </Card>
+                <div style={styles.FormButtonGroup}>
+                    <Button variant = 'contained' style={{color: 'white', backgroundcolor: '#5aac44'}}>
+                    {buttonTitle}{''}
+                    </Button>
+                    <Icon style={{marginLeft: 8, cursor:'pointer'}}>close</Icon>
+                </div>
+        </div>
     }
 
     render(){
@@ -55,6 +102,12 @@ const styles ={
         height: 36,
         width: 272,
         paddingLeft: 10
+    },
+
+    FormButtonGroup: {
+        marginTop: 8,
+        display: "flex",
+        alignItems: 'center'
     }
 }
 
